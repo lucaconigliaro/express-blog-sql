d0daa7b7033f9779b69d43f29a46a51da6bd3e6b
lucaconigliaro <lucaconigliaro1@gmail.com>
2025-01-22 15:22:31 +0100

    Milestone 2

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,6 +1,20 @@
+const connection = require("../data/db")
 
 const index = (req, res) => {
-   
+    const sql = "SELECT * FROM `posts`";
+
+    connection.query(sql, (err, results) => {
+        if (err) {
+            return res.status(500).json({
+                message: "Errore interno del server"
+            })
+        } else {
+            return res.status(200).json({
+                status: "success",
+                data: results,
+            });
+        }
+    });
 };
 
 const show = (req, res) => {
